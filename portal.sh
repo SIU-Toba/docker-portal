01f65d4cd7d04148721b744cde961e14c51c41c6
Sebastian Marconi <sebastianmarconi@gmail.com>
2015-06-12 21:03:40 -0300

    Faltaba linkear al container de postgres

--- portal.sh
+++ portal.sh
@@ -2,9 +2,9 @@
 #Se espera que haya un volumen compartido con el codigo en /var/local/portal
 
 function change_line {
-	sed "s/$1/$2/" $3 > $3.tmp
-	mv $3 $3.old
-	mv $3.tmp $3
+	sed "s/$1/$2/" "$3" > "$3.tmp"
+	mv "$3" "$3.old"
+	mv "$3.tmp" "$3"
 }
 
 
@@ -18,10 +18,13 @@ cd $PATH_PORTAL
 PATH_PARAMETERS="$PATH_PORTAL/app/config/parameters.yml"
 if [ ! -f "$PATH_PARAMETERS" ]; then
 	cp "$PATH_PARAMETERS.dist" "$PATH_PARAMETERS"
-	change_line "database_driver:   pdo_mysql" 	"database_driver:   pdo_pgsql" 		$PATH_PARAMETERS
-	change_line "database_host:     127.0.0.1" 	"database_host:     pg" 			$PATH_PARAMETERS
-	change_line "database_user:     root" 		"database_user:     postgres" 		$PATH_PARAMETERS
-	change_line "database_password: ~" 			"database_password: postgres" 		$PATH_PARAMETERS
+	change_line 'database_driver:   pdo_mysql' 	'database_driver:   pdo_pgsql' 					"$PATH_PARAMETERS"
+	change_line 'database_host:     127.0.0.1' 	'database_host:     pg' 						"$PATH_PARAMETERS"
+	change_line 'database_user:     root' 		'database_user:     postgres' 					"$PATH_PARAMETERS"
+	change_line 'database_port:     ~' 			'database_port:     5432'	 					"$PATH_PARAMETERS"
+	change_line 'database_password: ~' 			'database_password: postgres' 					"$PATH_PARAMETERS"
+	change_line 'database_password: ~' 			'database_password: postgres' 					"$PATH_PARAMETERS"
+	change_line 'session_memcached_host: localhost' 'session_memcached_host: memcached' 		"$PATH_PARAMETERS"
 fi
 
 if [ -z "$SYMFONY_ENV" ]; then
